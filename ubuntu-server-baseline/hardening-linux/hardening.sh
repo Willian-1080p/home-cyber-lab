4e3e6c33edabf0fbadea76a3cb0c7e2a71aa7312
wellington-infosec <santossilvawellington222@gmail.com>
2026-02-04 13:49:48 -0300

    Update hardening.sh

--- ubuntu-server-baseline/hardening-linux/hardening.sh
+++ ubuntu-server-baseline/hardening-linux/hardening.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+set -e
+
+echo "[+] Desabilitando serviços desnecessários"
+systemctl disable avahi-daemon || true
+systemctl disable cups || true
+
+echo "[+] SSH Hardening"
+cp sshd_config.hardened /etc/ssh/sshd_config
+systemctl restart ssh
+
+echo "[+] Instalando Fail2Ban"
+apt install -y fail2ban
+systemctl enable fail2ban
 
