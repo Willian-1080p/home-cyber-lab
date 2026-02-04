44b1252c765c20c16760d9e4756ef7c34a13fa3e
wellington-infosec <santossilvawellington222@gmail.com>
2026-02-04 13:50:03 -0300

    Update audit-only.sh

--- ubuntu-server-baseline/hardening-linux/audit-only.sh
+++ ubuntu-server-baseline/hardening-linux/audit-only.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+echo "[AUDIT] Serviços ativos:"
+systemctl list-units --type=service --state=running
+
+echo "[AUDIT] Portas abertas:"
+ss -tulnp
+
+echo "[AUDIT] Usuários com shell:"
+awk -F: '$7 ~ /bash/' /etc/passwd
 
