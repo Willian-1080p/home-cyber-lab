8b1922b3f52a6e768ab8089eb95588132fee1124
wellington-infosec <santossilvawellington222@gmail.com>
2026-02-04 13:44:08 -0300

    Update domain-hardening.ps1

--- Windows-server-baseline/active-directory-baseline/domain-hardening.ps1
+++ Windows-server-baseline/active-directory-baseline/domain-hardening.ps1
@@ -1,1 +1,12 @@
+<#
+ Script de apoio para hardening em ambiente AD
+ Não substitui GPO – apenas complementa
+#>
+
+Write-Host "[+] Forçando atualização de GPO"
+gpupdate /force
+
+Write-Host "[+] Habilitando auditoria avançada"
+auditpol /set /subcategory:"Logon" /success:enable /failure:enable
+auditpol /set /subcategory:"Account Lockout" /success:enable /failure:enable
 
